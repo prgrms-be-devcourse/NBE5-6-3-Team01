7bf82415032cbd3a0beb12be13104c651fc72e67
inseon <dlsline0611@gmail.com>
2025-06-17 09:45:48 +0900

    feat: JWT 인증 및 Redis 연동 설정 추가

--- src/main/java/com/grepp/synapse4/infra/config/SecurityConfig.java
+++ src/main/java/com/grepp/synapse4/infra/config/SecurityConfig.java
@@ -1,123 +1,105 @@
 package com.grepp.synapse4.infra.config;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import static org.springframework.http.HttpMethod.POST;
+
+import com.grepp.synapse4.infra.auth.token.JwtAuthenticationEntryPoint;
+import com.grepp.synapse4.infra.auth.token.filter.AuthExceptionFilter;
+import com.grepp.synapse4.infra.auth.token.filter.JwtAuthenticationFilter;
+import com.grepp.synapse4.infra.auth.token.filter.LogoutFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-
-    private final UserDetailsService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
-
-    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        this.userDetailsService = userDetailsService;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return (HttpServletRequest request, HttpServletResponse response, Authentication auth) -> {
-            boolean isAdmin = auth.getAuthorities().stream()
-                    .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
-            if (isAdmin) {
-                response.sendRedirect("/admin/users");
-            } else {
-                response.sendRedirect("/login-redirect");
-            }
-        };
-    }
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthExceptionFilter authExceptionFilter;
+    private final JwtAuthenticationEntryPoint entryPoint;
+    private final LogoutFilter logoutFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authenticationProvider(daoAuthenticationProvider())
-                .authorizeHttpRequests(auth -> auth
-                        // admin
-                        .requestMatchers(
-                                "/admin/signin", "/admin/signup",
-                                "/img/**", "/css/**"
-                        ).permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+            // CSRF, Form Login, HTTP Basic 인증 비활성화
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
 
-                        // user
-                        .requestMatchers("/css/**", "/js/**", "/img/**","/static/**").permitAll()
-                        .requestMatchers(
-                                "/", "/user/signin", "/user/signup",
-                                "/user/find-id", "/user/find-id/result",
-                                "/user/find-password", "/user/find-password/result",
-                                "/search/**", "/curation/**",
-                                "/restaurant/**", "/ranking/**"
-                        ).permitAll()
-                        .requestMatchers(
-                                "api/bookmarks/toggle/**","/api/users/**",
-                                "/recommend/**", "/meetings/**","/edit-prefer/**",
-                                "/mypage/**", "/bookmarks/**","/surveys/**"
-                        ).authenticated()
-                        .anyRequest().permitAll()
-                )
-                .formLogin(form -> form
-                        .loginPage("/user/signin")
-                        .loginProcessingUrl("/signin")
-                        .usernameParameter("userAccount")
-                        .passwordParameter("password")
-                        .successHandler(successHandler())
-                        .failureUrl("/user/signin?error=true")
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                )
-                .exceptionHandling(ex -> ex
-                        .defaultAuthenticationEntryPointFor(
-                                new LoginUrlAuthenticationEntryPoint("/admin/signin"),
-                                new AntPathRequestMatcher("/admin/**")
-                        )
-                        .defaultAuthenticationEntryPointFor(
-                                new LoginUrlAuthenticationEntryPoint("/user/signin"),
-                                new NegatedRequestMatcher(new AntPathRequestMatcher("/admin/**"))
-                        )
-                        .accessDeniedPage("/")
+            // 세션을 사용하지 않으므로 STATELESS로 설정
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
-                );
+            .authorizeHttpRequests(auth -> auth
+                // 관리자(admin)
+                .requestMatchers(
+                    "/admin/signin", "/admin/signup", "/img/**", "/css/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                // 공개
+                .requestMatchers("/", "/css/**", "/js/**", "/img/**", "/static/**").permitAll()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers(POST, "/api/auth/logout").permitAll()
+                .requestMatchers("/user/**", "/search/**", "/curation/**",
+                    "/restaurant/**", "/ranking/**").permitAll()
+
+                // 인증 필요한 사용자 경로
+                .requestMatchers(
+                    "api/bookmarks/toggle/**", "/api/users/**", "/api/user/**",
+                    "/recommend/**", "/meetings/**", "/edit-prefer/**",
+                    "/mypage/**", "/bookmarks/**", "/surveys/**"
+                ).authenticated()
+
+                .anyRequest().permitAll()
+            )
+            // JWT 인증 실패 시 처리할 EntryPoint 설정
+            .exceptionHandling(ex -> ex.authenticationEntryPoint(entryPoint))
+
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(authExceptionFilter, JwtAuthenticationFilter.class)
+            .addFilterBefore(logoutFilter, AuthExceptionFilter.class);
 
         return http.build();
-
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
+        throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Collections.singletonList(
+            "http://localhost:8080"
+        ));
+
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
 
